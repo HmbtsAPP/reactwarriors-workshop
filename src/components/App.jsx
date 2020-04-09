64d4800d8c6ca4320df6560cb8b3bf0a148cae93
HmbtS <alex@arsyonov.org.ua>
2020-04-09 20:14:37 +0300

    Проверка, какой респонс мы получили. Но без списка фильмов. То что мы ожидаем нет.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -21,8 +21,8 @@ class App extends React.Component {
 
   componentDidMount() {
     console.log("didMount");
-    fetch(`${API_URL}/discover/movie?api_key=${API_KEY_3}`).then(() => {
-      console.log("then");
+    fetch(`${API_URL}/discover/movie?api_key=${API_KEY_3}`).then((data) => {
+      console.log("then", data);
     });
     console.log("after fetch");
   }
