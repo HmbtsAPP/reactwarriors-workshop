12f07dc3a25cc946f5968236f3fe6e52d23fed68
HmbtS <alex@arsyonov.org.ua>
2020-04-09 20:23:35 +0300

    После fetch может быть несколько then. Мы передали дальше данные, но всеравно ими воспользоваться не сможем пока.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -21,9 +21,14 @@ class App extends React.Component {
 
   componentDidMount() {
     console.log("didMount");
-    fetch(`${API_URL}/discover/movie?api_key=${API_KEY_3}`).then((data) => {
-      console.log("then", data.json);
-    });
+    fetch(`${API_URL}/discover/movie?api_key=${API_KEY_3}`)
+      .then((response) => {
+        console.log("then");
+        response.json();
+      })
+      .then((data) => {
+        console.log("data", data);
+      });
     console.log("after fetch");
   }
 
