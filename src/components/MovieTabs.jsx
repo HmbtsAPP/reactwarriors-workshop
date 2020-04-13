8d4f9258049329ebacb8258d215a38554ede6fa2
HmbtS <alex@arsyonov.org.ua>
2020-04-13 19:46:25 +0300

    Пример для вылавлиявания события перед изменением props и state

--- src/components/MovieTabs.jsx
+++ src/components/MovieTabs.jsx
@@ -1,60 +1,65 @@
 import React from "react";
 
 class MovieTabs extends React.Component {
+  componentWillReceiveProps(nextProps, nextState) {
+    console.log("WillReceiveProps");
+    console.log("nextProps sort_by", nextProps.sort_by);
+    console.log("prevProps sort_by", this.props.sort_by);
+  }
   render() {
     const { sort_by, updateSortBy } = this.props;
 
-  //   const handleClick = (value) => {
-  //       return (event) => {
-  //     updateSortBy(value);
-  //   };}
+    //   const handleClick = (value) => {
+    //       return (event) => {
+    //     updateSortBy(value);
+    //   };}
 
-  const handleClick = (value) => () => {
-    updateSortBy(value);
-  };
+    const handleClick = (value) => () => {
+      updateSortBy(value);
+    };
 
-  const getClassLink = (value) => {
-    return `nav-link ${sort_by === value ? "active" : ""}`;
-  };
+    const getClassLink = (value) => {
+      return `nav-link ${sort_by === value ? "active" : ""}`;
+    };
 
-  return (
-    <ul className="tabs nav nav-pills">
-      <li className="nav-item">
-        <div
-          //   className={`nav-link ${
-          //     sort_by === "popularity.desc" ? "active" : ""
-          //   }`}
-          className={getClassLink("popularity.desc")}
-          onClick={handleClick("popularity.desc")}
-          // (event) => {
-          //     updateSortBy("popularity.desc")
-          // }
-        >
-          Popularity desc
-        </div>
-      </li>
-      <li className="nav-item">
-        <div
-          className={getClassLink("revenue.desc")}
-          onClick={handleClick("revenue.desc")}
-        >
-          Revenue desc
-        </div>
-      </li>
-      <li className="nav-item">
-        <div
-          className={getClassLink("vote_average.desc")}
-          onClick={handleClick("vote_average.desc")}
-        >
-          Vote average desc
-        </div>
-      </li>
-    </ul>
-  );
+    return (
+      <ul className="tabs nav nav-pills">
+        <li className="nav-item">
+          <div
+            //   className={`nav-link ${
+            //     sort_by === "popularity.desc" ? "active" : ""
+            //   }`}
+            className={getClassLink("popularity.desc")}
+            onClick={handleClick("popularity.desc")}
+            // (event) => {
+            //     updateSortBy("popularity.desc")
+            // }
+          >
+            Popularity desc
+          </div>
+        </li>
+        <li className="nav-item">
+          <div
+            className={getClassLink("revenue.desc")}
+            onClick={handleClick("revenue.desc")}
+          >
+            Revenue desc
+          </div>
+        </li>
+        <li className="nav-item">
+          <div
+            className={getClassLink("vote_average.desc")}
+            onClick={handleClick("vote_average.desc")}
+          >
+            Vote average desc
+          </div>
+        </li>
+      </ul>
+    );
   }
 }
 // const MovieTabs = (props) => {
-  
+
 // };
 
 export default MovieTabs;
